d655e6e675c918e1a032b6a3148897cc7c01e072
Eji <JerryAJ5410@outlook.com>
2019-09-15 17:09:14 +0800

    ui update

--- CVPlatform/App_ui/ViewModels/MainViewModel.cs
+++ CVPlatform/App_ui/ViewModels/MainViewModel.cs
@@ -1,17 +1,62 @@
 ﻿using DevExpress.Mvvm;
 using DevExpress.Mvvm.DataAnnotations;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace App_ui.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        #region Public field
+
+        /// <summary>
+        /// 当前图片路径
+        /// </summary>
+        public string CurrImagePath
+        {
+            get => GetProperty(() => CurrImagePath);
+            set => SetProperty(() => CurrImagePath, value, () =>
+            {
+                CurrBitmapImage = new BitmapImage(new Uri(CurrImagePath));
+            });
+        }
+
+        public BitmapImage CurrBitmapImage
+        {
+            get => GetProperty(() => CurrBitmapImage);
+            set => SetProperty(() => CurrBitmapImage, value);
+        }
+
+        #endregion
+
         #region Command
+
+        /// <summary>
+        /// 命令：打开新图片
+        /// </summary>
+        [Command]
+        public void OpenNewImageCommand()
+        {
+            OpenFileDialog imgFileDialog = new OpenFileDialog
+            {
+                Title = "Select a picture",
+                Filter = "All supported graphics|*.bmp;*.jpg;*.jpeg;*.png|" +
+                            "Bitmap (*.bmp)|*.bmp|" +
+                            "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                            "Portable Network Graphic (*.png)|*.png"
+            };
+            bool? ret = imgFileDialog.ShowDialog();
+            if (ret.HasValue && ret.Value)
+            {
+                CurrImagePath = imgFileDialog.FileName;
+            }
+        }
 
         /// <summary>
         /// 命令：显示关于对话框
