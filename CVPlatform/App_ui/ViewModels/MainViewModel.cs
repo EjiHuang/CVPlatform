8edba5fc5e14fa0b54f693ce43fc58f3daf29e2e
Eji <JerryAJ5410@outlook.com>
2019-09-15 11:28:37 +0800

    初步UI确定

---
+++ CVPlatform/App_ui/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App_ui.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        #region Command
+
+        /// <summary>
+        /// 命令：显示关于对话框
+        /// </summary>
+        [Command]
+        public void ShowAboutDialogCommand()
+        {
+            MessageBox.Show("Test");
+        }
+
+        #endregion
+    }
+}
